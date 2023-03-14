8a2b0605cd499b46d4e154e5cbc10a4da0cd92ef
Christian Nudo <45156056+cfn96@users.noreply.github.com>
2023-03-14 17:47:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,72 @@ from kivymd.app import MDApp
 from kivymd.uix.list import TwoLineListItem
 from kivymd.uix.button import MDFloatingActionButton
 from kivymd.uix.list import MDList
+from kivymd.uix.scrollview import MDScrollView
+from kivymd.uix.segmentedcontrol import MDSegmentedControl, MDSegmentedControlItem
 from db_init import loadrecord
 from kivy.uix.screenmanager import Screen, ScreenManager
-
 
 # load customers
 customer_record = loadrecord("customer")
 
 
+class WindowManager(ScreenManager):
+    def __init__(self, **kwargs):
+        super(WindowManager, self).__init__(**kwargs)
+
+
 class MainMenu(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(MainMenu, self).__init__(**kwargs)
+        self.current_filter = MDSegmentedControlItem(
+            text="Current",
+        )
+        self.paid_filter = MDSegmentedControlItem(
+            text="Paid",
+        )
+        self.all_filter = MDSegmentedControlItem(
+            text="All",
+        )
+        self.segmented_control = MDSegmentedControl(
+            self.current_filter,
+            self.paid_filter,
+            self.all_filter,
+            pos_hint={"center_x": 0.5, "center_y": 0.9}
+        )
+
+        self.add_widget(self.segmented_control)
+        self.segmented_control.bind(
+            on_active=lambda x, y: self.print_selected(self.segmented_control.current_active_segment))
+
+        list_scroll_view = MDScrollView(
+            pos_hint={"center_x": 0.5, "center_y": 0.35}
+        )
+        listing = MDList()
+        for customer in customer_record:
+            record_list = TwoLineListItem(
+                text=customer[1],
+                secondary_text='{:.2f}'.format(customer[2])
+            )
+            listing.add_widget(record_list)
+            # record_list.bind(
+            #     on_release=lambda record_list: self.print_selected(
+            #         record_list)
+            # )
+            record_list.bind(
+                on_release=lambda x: self.updateform()
+            )
+
+        list_scroll_view.add_widget(listing)
+        self.add_widget(list_scroll_view)
+
+    def print_selected(self, widget):
+        print(widget.text)
+
+    def updateform(self):
+        self.manager.current = "update_form"
 
 
-class SettingsMenu(Screen):
+class UpdateForm(Screen):
     pass
 
 
@@ -27,21 +80,6 @@ class MainApp(MDApp):
         self.theme_cls.primary_palette = "Teal"
         self.theme_cls.theme_style = "Dark"
         return Builder.load_file("main.kv")
-
-    def on_start(self):
-        for customer in customer_record:
-            record_list = TwoLineListItem(text=customer[1],
-                                          secondary_text='{:.2f}'.format(customer[2]))
-            record_list.bind(
-                on_release=lambda record_list=record_list: self.print_id(record_list))
-
-            record_list_widget = self.root.ids.container.add_widget(
-                record_list)
-
-        # return super().on_start()
-
-    def print_id(self, widget):
-        print(widget.text)
 
 
 if __name__ == "__main__":
